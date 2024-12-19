b43db593b5b0e7f8c9c4ad3e5d2598b8dbf91ad4
Marc Charest <marccharest@microsoft.com>
2024-12-18 17:18:37 -0800

    Adde e-s extensions for exodus files
    These are used to represent series of data accross multiple time steps

--- libsrc/mesh/file_type.cpp
+++ libsrc/mesh/file_type.cpp
@@ -10,7 +10,7 @@ file_type_t get_file_type(const std::string & str )
   size_t cnt = 0;
   while ( i != std::string::npos ) {
     auto ext = base.substr(i+1, base.length()-1);
-    if ( ext == "g" || ext == "exo" ) {
+    if ( ext == "g" || ext == "exo" || ext == "e-s" ) {
       if ( cnt != 0 ) return file_type_t::partitioned_exodus;
       else            return file_type_t::exodus;
     }
